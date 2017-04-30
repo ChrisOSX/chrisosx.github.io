3bbed59d47e535cc379e3f4647486f155195353e
ChrisOSX <home@Chriss-MacBook-Pro.local>
2017-04-30 19:12:40 -0400

    Update

--- depictions/resources/js/iosver.js
+++ depictions/resources/js/iosver.js
@@ -11,9 +11,6 @@ ver = iOSversion();
 if (ver[0] >= 8) {
 	document.getElementById('version').innerHTML='<p><strong>✓</strong> Your device is supported.</p>';
 }
-if (ver[0] >= 10) {
-	document.getElementById('version').innerHTML='<body style="background-color:#ff0000"><p style="color:#ff5050"><strong>Ive noticed youre on iOS 10.</strong><br>Please submit any bugs for this tweak/theme.</p></body';
-}
 else {
 	document.getElementById('version').innerHTML='<p><strong>x</strong> Your device is not supported.</p>';
 }
